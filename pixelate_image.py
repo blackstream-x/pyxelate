d74c0c4d6554870e416d1869f6234ac054271058
blackstream-x <rainer@blackstream.de>
2021-08-30 16:48:34 +0200

    Add requirements.txt and first script skeleton

---
+++ pixelate_image.py
@@ -0,0 +1,623 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+
+pixelate_image.py
+
+Pixelate a part of an image
+(Tkinter-based GUI assistant supporting Nautilus script integration)
+
+"""
+
+
+import argparse
+import logging
+import mimetypes
+import os
+import pathlib
+# import re
+import sys
+import tkinter
+# import webbrowser
+
+from tkinter import filedialog
+# from tkinter import messagebox
+# from tkinter import ttk
+
+# local modules
+
+# to add: import pixelate
+
+#
+# Constants
+#
+
+
+SCRIPT_NAME = 'Partially pixelate an image'
+HOMEPAGE = 'https://github.com/blackstream-x/pyxelate'
+MAIN_WINDOW_TITLE = 'pyxelate: partiyllay pixelate an image'
+
+SCRIPT_PATH = pathlib.Path(sys.argv[0])
+# Follow symlinks
+if SCRIPT_PATH.is_symlink():
+    SCRIPT_PATH = SCRIPT_PATH.readlink()
+#
+
+LICENSE_PATH = SCRIPT_PATH.parent / 'LICENSE'
+try:
+    LICENSE_TEXT = LICENSE_PATH.read_text()
+except OSError as error:
+    LICENSE_TEXT = '(License file is missing: %s)' % error
+#
+
+VERSION_PATH = SCRIPT_PATH.parent / 'version.txt'
+try:
+    VERSION = VERSION_PATH.read_text().strip()
+except OSError as error:
+    VERSION = '(Version file is missing: %s)' % error
+#
+
+# Phases
+CHOOSE_IMAGE = 'choose_image'
+SELECT_AREA = 'select_area'
+
+PHASES = (
+    CHOOSE_IMAGE,
+    SELECT_AREA)
+
+PANEL_NAMES = {
+    SELECT_AREA: 'Select area to be pixelated'}
+
+
+#
+# Helper Functions
+#
+
+
+...
+
+
+#
+# Classes
+#
+
+
+class Namespace(dict):
+
+    # pylint: disable=too-many-instance-attributes
+
+    """A dict subclass that exposes its items as attributes.
+
+    Warning: Namespace instances only have direct access to the
+    attributes defined in the visible_attributes tuple
+    """
+
+    visible_attributes = ('items', )
+
+    def __repr__(self):
+        """Object representation"""
+        return '{0}({1})'.format(
+            type(self).__name__,
+            super().__repr__())
+
+    def __dir__(self):
+        """Members sequence"""
+        return tuple(self)
+
+    def __getattribute__(self, name):
+        """Access a visible attribute
+        or return an existing dict member
+        """
+        if name in type(self).visible_attributes:
+            return object.__getattribute__(self, name)
+        #
+        try:
+            return self[name]
+        except KeyError as error:
+            raise AttributeError(
+                '{0!r} object has no attribute {1!r}'.format(
+                    type(self).__name__, name)) from error
+        #
+
+    def __setattr__(self, name, value):
+        """Set an attribute"""
+        self[name] = value
+
+    def __delattr__(self, name):
+        """Delete an attribute"""
+        del self[name]
+
+
+class ModalDialog(tkinter.Toplevel):
+
+    """Adapted from
+    <https://effbot.org/tkinterbook/tkinter-dialog-windows.htm>
+    """
+
+    def __init__(self,
+                 parent,
+                 content,
+                 title=None,
+                 cancel_button=True):
+        """Create the toplevel window and wait until the dialog is closed"""
+        super().__init__(parent)
+        self.transient(parent)
+        if title:
+            self.title(title)
+        #
+        self.parent = parent
+        self.initial_focus = self
+        self.body = tkinter.Frame(self)
+        self.create_content(content)
+        self.body.grid(padx=5, pady=5, sticky=tkinter.E + tkinter.W)
+        self.create_buttonbox(cancel_button=cancel_button)
+        self.grab_set()
+        self.protocol("WM_DELETE_WINDOW", self.action_cancel)
+        self.initial_focus.focus_set()
+        self.wait_window(self)
+
+    def create_content(self, content):
+        """Add content to body"""
+        for (heading, paragraph) in content:
+            heading_area = tkinter.Label(
+                self.body,
+                text=heading,
+                font=(None, 11, 'bold'),
+                justify=tkinter.LEFT)
+            heading_area.grid(sticky=tkinter.W, padx=5, pady=10)
+            text_area = tkinter.Label(
+                self.body,
+                text=paragraph,
+                justify=tkinter.LEFT)
+            text_area.grid(sticky=tkinter.W, padx=5, pady=5)
+        #
+
+    def create_buttonbox(self, cancel_button=True):
+        """Add standard button box."""
+        box = tkinter.Frame(self)
+        button = tkinter.Button(
+            box,
+            text="OK",
+            width=10,
+            command=self.action_ok,
+            default=tkinter.ACTIVE)
+        button.grid(padx=5, pady=5, row=0, column=0, sticky=tkinter.W)
+        if cancel_button:
+            button = tkinter.Button(
+                box,
+                text="Cancel",
+                width=10,
+                command=self.action_cancel)
+            button.grid(padx=5, pady=5, row=0, column=1, sticky=tkinter.E)
+        #
+        self.bind("<Return>", self.action_ok)
+        box.grid(padx=5, pady=5, sticky=tkinter.E + tkinter.W)
+
+    #
+    # standard button semantics
+
+    def action_ok(self, event=None):
+        """Clean up"""
+        del event
+        self.withdraw()
+        self.update_idletasks()
+        self.action_cancel()
+
+    def action_cancel(self, event=None):
+        """Put focus back to the parent window"""
+        del event
+        self.parent.focus_set()
+        self.destroy()
+
+
+class InfoDialog(ModalDialog):
+
+    """Info dialog,
+    instantiated with a seriess of (heading, paragraph) tuples
+    after the parent window
+    """
+
+    def __init__(self,
+                 parent,
+                 *content,
+                 title=None):
+        """..."""
+        super().__init__(parent, content, title=title, cancel_button=False)
+
+
+class UserInterface():
+
+    """GUI using tkinter"""
+
+    with_border = dict(
+        borderwidth=2,
+        padx=5,
+        pady=5,
+        relief=tkinter.GROOVE)
+    grid_fullwidth = dict(
+        padx=4,
+        pady=2,
+        sticky=tkinter.E + tkinter.W)
+
+    # pylint: disable=attribute-defined-outside-init
+
+    def __init__(self, file_path):
+        """Build the GUI"""
+        self.main_window = tkinter.Tk()
+        self.main_window.title(MAIN_WINDOW_TITLE)
+        self.variables = Namespace(
+            errors=[],
+            file_name=tkinter.StringVar(),
+            image=None,
+            original_path=file_path,
+            target_path=tkinter.StringVar(),
+            pixel_size=tkinter.IntVar(),
+            shape=tkinter.StringVar(),
+            shape_height=tkinter.IntVar(),
+            shape_width=tkinter.IntVar(),
+            panel_display=tkinter.StringVar())
+        self.widgets = Namespace(
+            action_area=None,
+            buttons_area=None)
+        overview_frame = tkinter.Frame(self.main_window)
+        file_label = tkinter.Label(
+            overview_frame,
+            text='File:')
+        file_label.grid(
+            padx=4, pady=2, row=0, column=0, sticky=tkinter.W)
+        selected_file = tkinter.Entry(
+            overview_frame,
+            width=60,
+            state=tkinter.DISABLED,
+            textvariable=self.variables.file_name)
+        selected_file.grid(
+            padx=4, pady=2, row=0, column=1, sticky=tkinter.W)
+        choose_button = tkinter.Button(
+            overview_frame,
+            text='Choose another …',
+            command=self.do_choose_image)
+        choose_button.grid(
+            padx=4, pady=4, row=0, column=2, sticky=tkinter.W)
+        panel_display = tkinter.Label(
+            overview_frame,
+            textvariable=self.variables.panel_display,
+            justify=tkinter.LEFT)
+        panel_display.grid(
+            padx=4, pady=4, row=1, column=0, columnspan=3, sticky=tkinter.W)
+        overview_frame.grid(**self.grid_fullwidth)
+        self.do_choose_image(
+            keep_existing=True,
+            quit_on_empty_choice=True)
+        self.main_window.mainloop()
+
+    def do_choose_image(self,
+                        keep_existing=False,
+                        preset_path=None,
+                        quit_on_empty_choice=False):
+        """Choose an image via file dialog"""
+        if preset_path:
+            if not preset_path.is_dir():
+                initial_dir = str(preset_path.parent)
+            #
+        elif self.variables.original_path:
+            initial_dir = str(self.variables.original_path.parent)
+        else:
+            initial_dir = os.getcwd()
+        #
+        while True:
+            if not keep_existing or self.variables.original_path is None:
+                selected_file = filedialog.askopenfilename(
+                    initialdir=initial_dir)
+                if not selected_file:
+                    if quit_on_empty_choice:
+                        self.quit()
+                    #
+                    return
+                #
+                self.variables.original_path = pathlib.Path(
+                    selected_file)
+            #
+            file_path = self.variables.original_path
+            # TODO: check for an image mime type and retry
+            # if the selected file is not an image
+            # if not mimetype is imagemimetype: continue
+            #
+            # TODO: read image data
+            # self.varibales.image = pixelate.imageData(file_path)
+            self.variables.file_name = file_path.name
+            break
+        #
+
+    def do_select_area(self):
+        """..."""
+        ...
+
+    def panel_select_area(self):
+        """Show the image on a canvas and let
+        the user select the area to be pixelated
+        """
+# =============================================================================
+#         label_grid = dict(
+#             column=0,
+#             padx=4,
+#             sticky=tkinter.E)
+#         value_grid = dict(
+#             column=1,
+#             columnspan=3,
+#             padx=4,
+#             sticky=tkinter.W)
+# =============================================================================
+        shape_frame = tkinter.Frame(
+            self.widgets.action_area,
+            **self.with_border)
+        ...
+        shape_frame.grid(**self.grid_fullwidth)
+        image_frame = tkinter.Frame(
+            self.widgets.action_area,
+            **self.with_border)
+        ...
+        image_frame.grid(**self.grid_fullwidth)
+
+    def next_action(self):
+        """Execute the next action"""
+        next_index = PHASES.index(self.variables.current_panel) + 1
+        try:
+            next_phase = PHASES[next_index]
+        except IndexError:
+            self.variables.errors.append(
+                'Phase number #%s out of range' % next_index)
+        #
+        try:
+            action_method = getattr(self, 'do_%s' % next_phase)
+        except AttributeError:
+            self.variables.errors.append(
+                'Action method for phase #%s (%r)'
+                ' has not been defined yet' % (next_index, next_phase))
+        except NotImplementedError:
+            self.variables.errors.append(
+                'Action method for phase #%s (%r)'
+                ' has not been implemented yet' % (next_index, next_phase))
+        else:
+            self.variables.current_phase = next_phase
+            action_method()
+        #
+
+    def next_panel(self):
+        """Execute the next action and go to the next panel"""
+        self.next_action()
+        self.__show_panel()
+
+    def previous_panel(self):
+        """Go to the next panel"""
+        phase_index = PHASES.index(self.variables.current_panel)
+        try:
+            rollback_method = getattr(
+                self,
+                'rollback_%s' % self.variables.current_panel)
+        except AttributeError:
+            self.variables.errors.append(
+                'Rollback method for phase #%s (%r)'
+                ' has not been defined yet' % (
+                    phase_index, self.variables.current_panel))
+        else:
+            self.variables.current_phase = PHASES[phase_index - 1]
+            try:
+                rollback_method()
+            except NotImplementedError:
+                self.variables.errors.append(
+                    'Rollback method for phase #%s (%r)'
+                    ' has not been implemented yet' % (
+                        phase_index, self.variables.current_panel))
+            #
+        #
+        self.__show_panel()
+
+    def quit(self, event=None):
+        """Exit the application"""
+        del event
+        self.main_window.destroy()
+
+    def rollback_select_area(self):
+        """Rollback area selection
+        (probably not useful in this context)
+        """
+        ...
+
+    def show_about(self):
+        """Show information about the application
+        in a modal dialog
+        """
+        InfoDialog(
+            self.main_window,
+            (SCRIPT_NAME,
+             'Version: {0}\nProject homepage: {1}'.format(
+                VERSION, HOMEPAGE)),
+            ('License:', LICENSE_TEXT),
+            title='About…')
+        #
+
+
+    def __show_errors(self):
+        """Show errors if there are any"""
+        if self.variables.errors:
+            errors_frame = tkinter.Frame(
+                self.widgets.action_area,
+                **self.with_border)
+            for message in self.variables.errors:
+                error_value = tkinter.Label(
+                    errors_frame,
+                    text=message,
+                    justify=tkinter.LEFT)
+                error_value.grid(
+                    padx=4,
+                    sticky=tkinter.W)
+            #
+            self.variables.errors.clear()
+            errors_frame.grid(**self.grid_fullwidth)
+        #
+
+    def __show_panel(self):
+        """Show a panel.
+        Add the "Previous", "Next", "Choose another relase",
+        "About" and "Quit" buttons at the bottom
+        """
+        for area in ('action_area', 'buttons_area'):
+            try:
+                self.widgets[area].grid_forget()
+            except AttributeError:
+                pass
+            #
+        #
+        self.widgets.action_area = tkinter.Frame(
+            self.main_window,
+            **self.with_border)
+        try:
+            panel_method = getattr(
+                self,
+                'panel_%s' % self.variables.current_phase)
+        except AttributeError:
+            self.variables.errors.append(
+                'Panel for Phase %r has not been implemented yet,'
+                ' going back to phase %r.' % (
+                    self.variables.current_phase,
+                    self.variables.current_panel))
+            self.variables.current_phase = self.variables.current_panel
+            panel_method = getattr(
+                self,
+                'panel_%s' % self.variables.current_phase)
+            self.variables.disable_next_button = False
+        else:
+            self.variables.current_panel = self.variables.current_phase
+        #
+        self.variables.panel_display.set(
+            '%s (panel %s of %s)' % (
+                PANEL_NAMES[self.variables.current_panel],
+                PHASES.index(self.variables.current_panel),
+                len(PHASES) - 1))
+        self.__show_errors()
+        panel_method()
+        self.widgets.action_area.grid(**self.grid_fullwidth)
+        #
+        self.widgets.buttons_area = tkinter.Frame(
+            self.main_window,
+            **self.with_border)
+        #
+        buttons_grid = dict(padx=5, pady=5, row=0)
+        if self.variables.current_phase in ():
+            previous_button_state = tkinter.NORMAL
+        else:
+            previous_button_state = tkinter.DISABLED
+        #
+        previous_button = tkinter.Button(
+            self.widgets.buttons_area,
+            text='\u25c1 Previous',
+            command=self.previous_panel,
+            state=previous_button_state)
+        previous_button.grid(column=0, sticky=tkinter.W, **buttons_grid)
+        #
+        if self.variables.disable_next_button or \
+                self.variables.current_phase == SELECT_AREA:
+            next_button_state = tkinter.DISABLED
+        else:
+            next_button_state = tkinter.NORMAL
+        #
+        self.variables.disable_next_button = False
+        next_button = tkinter.Button(
+            self.widgets.buttons_area,
+            text='\u25b7 Next',
+            command=self.next_panel,
+            state=next_button_state)
+        next_button.grid(column=1, sticky=tkinter.W, **buttons_grid)
+        about_button = tkinter.Button(
+            self.widgets.buttons_area,
+            text='About…',
+            command=self.show_about)
+        about_button.grid(column=3, sticky=tkinter.E, **buttons_grid)
+        quit_button = tkinter.Button(
+            self.widgets.buttons_area,
+            text='Quit',
+            command=self.quit)
+        quit_button.grid(column=4, sticky=tkinter.E, **buttons_grid)
+        self.widgets.buttons_area.columnconfigure(2, weight=100)
+        self.widgets.buttons_area.grid(**self.grid_fullwidth)
+
+
+#
+# Functions
+#
+
+
+def __get_arguments():
+    """Parse command line arguments"""
+    argument_parser = argparse.ArgumentParser(
+        description='Pixelate an area of an image')
+    argument_parser.set_defaults(loglevel=logging.INFO)
+    argument_parser.add_argument(
+        '-v', '--verbose',
+        action='store_const',
+        const=logging.DEBUG,
+        dest='loglevel',
+        help='Output all messages including debug level')
+    argument_parser.add_argument(
+        '-q', '--quiet',
+        action='store_const',
+        const=logging.WARNING,
+        dest='loglevel',
+        help='Limit message output to warnings and errors')
+    argument_parser.add_argument(
+        '-f', '--file',
+        type=pathlib.Path,
+        help='An image file')
+    argument_parser.add_argument(
+        'dummy',
+        nargs=argparse.REMAINDER)
+    return argument_parser.parse_args()
+
+
+def main(arguments=None):
+    """Main script function"""
+    selected_file = None
+    try:
+        loglevel = arguments.loglevel
+        selected_file = arguments.directory
+    except AttributeError:
+        loglevel = logging.WARNING
+    #
+    if selected_file and not selected_file.is_file():
+        selected_file = None
+    #
+    logging.basicConfig(
+        format='%(levelname)-8s\u2551 %(funcName)s → %(message)s',
+        level=loglevel)
+    try:
+        selected_names = os.environ['NAUTILUS_SCRIPT_SELECTED_FILE_PATHS']
+    except KeyError:
+        pass
+    else:
+        for name in selected_names.splitlines():
+            if name:
+                current_path = pathlib.Path(name)
+                if current_path.is_file():
+                    selected_file = current_path
+                    break
+                #
+            #
+        #
+    #
+    UserInterface(selected_file)
+
+
+if __name__ == '__main__':
+    # =========================================================================
+    # Workaround for unexpected behavior when called
+    # as a Nautilus script in combination with argparse
+    # =========================================================================
+    try:
+        sys.exit(main(__get_arguments()))
+    except Exception:
+        sys.exit(main())
+    #
+
+
+# vim: fileencoding=utf-8 ts=4 sts=4 sw=4 autoindent expandtab syntax=python:
